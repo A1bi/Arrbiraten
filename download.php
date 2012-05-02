7b70498577bb26e218f44c2ca68cdcc6052b99dd
Albrecht Oster <a.oster@online.de>
2012-05-02 13:32:21 +0200

    download photos

--- download.php
+++ download.php
@@ -1,7 +1,5 @@
 <?php
 include('include/main.php');
-kickGuests(true);
-
 loadComponent("pics");
 
 function correctForumPost($text) {
@@ -22,213 +20,227 @@ function correctText($text) {
 	return $text;
 }
 
-
-switch ($_GET['action']) {
+if ($_GET['action'] == "file") {
+	kickGuests();
+	if (!empty($_GET['id'])) {
+		$result = $_db->query('SELECT file FROM downloads WHERE id = ?', array($_GET['id']));
+		$file = $result->fetch();
+		$_db->query('UPDATE downloads SET downloads = downloads + 1 WHERE id = ?', array($_GET['id']));
+		
+		redirectTo($file['file']);
+	}
+			
+} else {
+	kickGuests(true);
 	
-	case "story":
-		$pics = new pics(0);
-		
-		$result = $_db->query('SELECT id, file, filename, subject, teacher FROM stories WHERE id = ?', array($_GET['id']));
-		$story = $result->fetch();
-		
-		$zip = new ZipArchive();
-		
-		if (!empty($story['id'])) {
-			// create zip file
-			$zipFile = "/media/".$story['id'].".zip";
-			$zip->open($_base.$zipFile, ZIPARCHIVE::OVERWRITE);
-			$root = $story['subject']." ".$story['teacher'];
-			$zip->addEmptyDir($root);
-			
-			// add story document
-			$parts = explode(".", $story['filename']);
-			$zip->addFile($_base."media/".$story['file'], $root."/bericht.".end($parts));
-			
-			// add all photos
-			$photos = $root."/pics";
-			$zip->addEmptyDir($photos);
-			$pics->setType(3);
-			$allPics = $pics->getAll($story['id']);
-			
-			$i = 0;
-			foreach ($allPics as $pic) {
-				$zip->addFile($_base."gfx/cache/pics/full/".$pic['pic'].".jpg", $photos."/".$i.".jpg");
-				$i++;
-			}
-			
-			$zip->close();
-			
-			$_db->query('UPDATE stories SET downloaded = ? WHERE id = ?', array(time(), $story['id']));
-			
-			// redirect to newly created file
-			redirectTo($zipFile);
-		}
-		
-		echo "Fehler!";
-		break;
-		
-	case "profiles":
-		$pics = new pics(4);
-		
-		if ($_GET['pics']) {
-			// create zip file
+	switch ($_GET['action']) {
+
+		case "story":
+			$pics = new pics(0);
+
+			$result = $_db->query('SELECT id, file, filename, subject, teacher FROM stories WHERE id = ?', array($_GET['id']));
+			$story = $result->fetch();
+
 			$zip = new ZipArchive();
-			$zipFile = "/media/profiles.zip";
-			$zip->open($_base.$zipFile, ZIPARCHIVE::OVERWRITE);
 
-			$root = "Steckbriefe";
+			if (!empty($story['id'])) {
+				// create zip file
+				$zipFile = "/media/".$story['id'].".zip";
+				$zip->open($_base.$zipFile, ZIPARCHIVE::OVERWRITE);
+				$root = $story['subject']." ".$story['teacher'];
+				$zip->addEmptyDir($root);
+
+				// add story document
+				$parts = explode(".", $story['filename']);
+				$zip->addFile($_base."media/".$story['file'], $root."/bericht.".end($parts));
+
+				// add all photos
+				$photos = $root."/pics";
+				$zip->addEmptyDir($photos);
+				$pics->setType(3);
+				$allPics = $pics->getAll($story['id']);
+
+				$i = 0;
+				foreach ($allPics as $pic) {
+					$zip->addFile($_base."gfx/cache/pics/full/".$pic['pic'].".jpg", $photos."/".$i.".jpg");
+					$i++;
+				}
+
+				$zip->close();
+
+				$_db->query('UPDATE stories SET downloaded = ? WHERE id = ?', array(time(), $story['id']));
+
+				// redirect to newly created file
+				redirectTo($zipFile);
+			}
+
+			echo "Fehler!";
+			break;
+
+		case "profiles":
+			$pics = new pics(4);
+
+			if ($_GET['pics']) {
+				// create zip file
+				$zip = new ZipArchive();
+				$zipFile = "/media/profiles.zip";
+				$zip->open($_base.$zipFile, ZIPARCHIVE::OVERWRITE);
+
+				$root = "Steckbriefe";
+				$zip->addEmptyDir($root);
+
+				$photos = $root."/pics";
+				$zip->addEmptyDir($photos);
+			}
+
+			$xml = new SimpleXMLElement("<people />");
+
+			$result = $_db->query('SELECT * FROM people ORDER BY firstname ASC');
+			while ($person = $result->fetch()) {
+				// add basic info
+				$personXML = $xml->addChild("person");
+				$personXML->addChild("firstname", $person['firstname']);
+				$personXML->addChild("lastname", $person['lastname']);
+
+				// add photos
+				$picsXML = $personXML->addChild("pics");
+				$pics->setType(4);
+				$allPics = $pics->getAll($person['user']);
+				$pics->setType(1);
+				$allPics = array_merge_recursive($allPics, $pics->getAll($person['user']));
+				foreach ($allPics as $pic) {
+					$picsXML->addChild("pic", $pic['pic']);
+					if ($_GET['pics']) {
+						$zip->addFile($_base."gfx/cache/pics/full/".$pic['pic'].".jpg", $photos."/".$pic['pic'].".jpg");
+					}
+				}
+
+				// add profile fields
+				$profile = "";
+				$fields = array(
+					"nick" => array(
+						"caption" => "Landratte"
+					),
+					"birthday" => array(
+						"caption" => "Segelt seit"
+					),
+					"location" => array(
+						"caption" => "Heimathafen"
+					),
+					"tutor" => array(
+						"caption" => "Captain"
+					),
+					"lks" => array(
+						"caption" => "Flagschiffe"
+					),
+					"goals" => array(
+						"caption" => "Nehme Kurs auf"
+					),
+					"hobbies" => array(
+						"caption" => "Rum und..."
+					),
+					"top" => array(
+						"caption" => "Günstiger Wind"
+					),
+					"flop" => array(
+						"caption" => "Flaute"
+					),
+					"saying" => array(
+						"caption" => "Piratenweisheit"
+					),
+					"greetings" => array(
+						"caption" => "Flaschenpost"
+					)
+				);
+				$result2 = $_db->query('SELECT field, value FROM profile_fields WHERE user = ?', array($person['user']));
+				$values = $_db->fetchAll($result2, "field");
+				$fieldsXML = $personXML->addChild("fields");
+				foreach ($fields as $field => $info) {
+					$value = correctText($values[$field]['value']);
+					if (empty($value) || $value == "-") continue;
+
+					if ($field == "birthday") {
+						$date = explode(".", $value);
+						for ($i = 0; $i < 2; $i++) {
+							if (strlen($date[$i])<2) $date[$i] = "0".$date[$i];
+						}
+						if (strlen($date[2])<3) $date[2] = "19".$date[$i];
+						$value = implode(".", $date);
+					}
+
+					$fieldXML = $fieldsXML->addChild("field");
+					$fieldXML->addChild("caption", $info['caption']);
+					$fieldXML->value = $value;
+				}
+
+				// add about
+				$about = "";
+				$result2 = $_db->query('SELECT post_text AS text FROM phpbb_posts WHERE topic_id = ? ORDER BY post_id ASC LIMIT 1, 999', array($person['topic']));
+				while ($post = $result2->fetch()) {
+					$text = correctText(correctForumPost($post['text']));
+					$about .= $text."\r";
+				}
+				$personXML->about = substr($about, 0, -1);
+
+			}
+
+			if ($_GET['pics']) {
+				// add xml file
+				$zip->addFromString($root."/people.xml", $xml->asXML());
+				$zip->close();
+
+				// redirect to newly created file
+				redirectTo($zipFile);
+
+			} else {
+				header("Content-type: text/xml");
+				echo $xml->asXML();
+			}
+
+			break;
+
+		case "topic":
+			header("Content-Type: text/html; charset=UTF-8");
+			$result = $_db->query('SELECT post_text AS text FROM phpbb_posts WHERE topic_id = ?', array($_GET['id']));
+			while ($post = $result->fetch()) {
+				$text = correctText(correctForumPost($post['text']));
+				echo $text."\r";
+			}
+
+			break;
+
+		case "names":
+			header("Content-Type: text/html; charset=UTF-8");
+			$result = $_db->query('SELECT firstname, lastname FROM people ORDER BY firstname ASC');
+			while ($person = $result->fetch()) {
+				echo $person['firstname'] . " " . $person['lastname'] . " - ";
+			}
+
+			break;
+
+		case "pics":	
+			$zip = new ZipArchive();
+
+			// create zip file
+			$zipFile = "/media/pics.zip";
+			$zip->open($_base.$zipFile, ZIPARCHIVE::OVERWRITE);
+			$root = "Collagenfotos";
 			$zip->addEmptyDir($root);
 
-			$photos = $root."/pics";
-			$zip->addEmptyDir($photos);
-		}
-			
-		$xml = new SimpleXMLElement("<people />");
-		
-		$result = $_db->query('SELECT * FROM people ORDER BY firstname ASC');
-		while ($person = $result->fetch()) {
-			// add basic info
-			$personXML = $xml->addChild("person");
-			$personXML->addChild("firstname", $person['firstname']);
-			$personXML->addChild("lastname", $person['lastname']);
-			
-			// add photos
-			$picsXML = $personXML->addChild("pics");
-			$pics->setType(4);
-			$allPics = $pics->getAll($person['user']);
-			$pics->setType(1);
-			$allPics = array_merge_recursive($allPics, $pics->getAll($person['user']));
-			foreach ($allPics as $pic) {
-				$picsXML->addChild("pic", $pic['pic']);
-				if ($_GET['pics']) {
-					$zip->addFile($_base."gfx/cache/pics/full/".$pic['pic'].".jpg", $photos."/".$pic['pic'].".jpg");
-				}
+			// add all photos
+			$result = $_db->query('SELECT pic FROM pics WHERE type = 2');
+			while ($pic = $result->fetch()) {
+				$zip->addFile($_base."gfx/cache/pics/full/".$pic['pic'].".jpg", $root."/".$pic['pic'].".jpg");
 			}
-			
-			// add profile fields
-			$profile = "";
-			$fields = array(
-				"nick" => array(
-					"caption" => "Landratte"
-				),
-				"birthday" => array(
-					"caption" => "Segelt seit"
-				),
-				"location" => array(
-					"caption" => "Heimathafen"
-				),
-				"tutor" => array(
-					"caption" => "Captain"
-				),
-				"lks" => array(
-					"caption" => "Flagschiffe"
-				),
-				"goals" => array(
-					"caption" => "Nehme Kurs auf"
-				),
-				"hobbies" => array(
-					"caption" => "Rum und..."
-				),
-				"top" => array(
-					"caption" => "Günstiger Wind"
-				),
-				"flop" => array(
-					"caption" => "Flaute"
-				),
-				"saying" => array(
-					"caption" => "Piratenweisheit"
-				),
-				"greetings" => array(
-					"caption" => "Flaschenpost"
-				)
-			);
-			$result2 = $_db->query('SELECT field, value FROM profile_fields WHERE user = ?', array($person['user']));
-			$values = $_db->fetchAll($result2, "field");
-			$fieldsXML = $personXML->addChild("fields");
-			foreach ($fields as $field => $info) {
-				$value = correctText($values[$field]['value']);
-				if (empty($value) || $value == "-") continue;
-				
-				if ($field == "birthday") {
-					$date = explode(".", $value);
-					for ($i = 0; $i < 2; $i++) {
-						if (strlen($date[$i])<2) $date[$i] = "0".$date[$i];
-					}
-					if (strlen($date[2])<3) $date[2] = "19".$date[$i];
-					$value = implode(".", $date);
-				}
-				
-				$fieldXML = $fieldsXML->addChild("field");
-				$fieldXML->addChild("caption", $info['caption']);
-				$fieldXML->value = $value;
-			}
-			
-			// add about
-			$about = "";
-			$result2 = $_db->query('SELECT post_text AS text FROM phpbb_posts WHERE topic_id = ? ORDER BY post_id ASC LIMIT 1, 999', array($person['topic']));
-			while ($post = $result2->fetch()) {
-				$text = correctText(correctForumPost($post['text']));
-				$about .= $text."\r";
-			}
-			$personXML->about = substr($about, 0, -1);
-			
-		}
-		
-		if ($_GET['pics']) {
-			// add xml file
-			$zip->addFromString($root."/people.xml", $xml->asXML());
+
 			$zip->close();
-			
+
 			// redirect to newly created file
 			redirectTo($zipFile);
+
+			break;
 			
-		} else {
-			header("Content-type: text/xml");
-			echo $xml->asXML();
-		}
-		
-		break;
-		
-	case "topic":
-		header("Content-Type: text/html; charset=UTF-8");
-		$result = $_db->query('SELECT post_text AS text FROM phpbb_posts WHERE topic_id = ?', array($_GET['id']));
-		while ($post = $result->fetch()) {
-			$text = correctText(correctForumPost($post['text']));
-			echo $text."\r";
-		}
-		
-		break;
-		
-	case "names":
-		header("Content-Type: text/html; charset=UTF-8");
-		$result = $_db->query('SELECT firstname, lastname FROM people ORDER BY firstname ASC');
-		while ($person = $result->fetch()) {
-			echo $person['firstname'] . " " . $person['lastname'] . " - ";
-		}
-		
-		break;
-		
-	case "pics":	
-		$zip = new ZipArchive();
-		
-		// create zip file
-		$zipFile = "/media/pics.zip";
-		$zip->open($_base.$zipFile, ZIPARCHIVE::OVERWRITE);
-		$root = "Collagenfotos";
-		$zip->addEmptyDir($root);
-
-		// add all photos
-		$result = $_db->query('SELECT pic FROM pics WHERE type = 2');
-		while ($pic = $result->fetch()) {
-			$zip->addFile($_base."gfx/cache/pics/full/".$pic['pic'].".jpg", $root."/".$pic['pic'].".jpg");
-		}
-
-		$zip->close();
-
-		// redirect to newly created file
-		redirectTo($zipFile);
-
-		break;
-		
+	}
+	
 }
 ?>
--- include/templates/index.tpl
+++ include/templates/index.tpl
@@ -1,41 +1,66 @@
 {include file="head.tpl"}
 <div class="navigation">
 	<div class="row">
-		<div class="box">
-			<a href="/forum" class="hl">Forum</a>
-			Alles Organisatorische, u.A. Rubriken für die Abizeitung.
-		</div>
-		<div class="box">
-			<a href="/lists/official" class="hl">Planung offizieller Teil</a>
-			Verschiedene Infos, die zur Planung der offiziellen Verabschiedung benötigt werden.
-		</div>
-		<div class="box">
-			<a href="/lists/song" class="hl">Einlauflied</a>
-			Sende dein persönliches Lied für die Zeugnisvergabe ein.
+		<div class="box photos">
+			<a href="/forum" class="hl">Fotos Download</a>
+			<table>
+				<tr class="head">
+					<td>Offizielle Feier</td>
+					<td>Abikonzert</td>
+				</tr>
+				<tr>
+					<td>
+						<p><a href="/download/1">Kirche und Verabschiedung</a> <span class="size">(1,81 GB)</span></p>
+						<p><a href="/download/2">Gesamtfotos der Stufe</a> <span class="size">(49,2 MB)</span></p>
+						<p><a href="/download/3">Gruppenfotos</a> <span class="size">(2,51 GB)</span></p>
+					</td>
+					<td class="concert">
+						<p><a href="/download/4">Proben</a> <span class="size">(1,02 GB)</span></p>
+						<p><a href="/download/5">Konzert</a> <span class="size">(2,81 GB)</span></p>
+					</td>
+				</tr>
+			</table>
+			<p>Danke an Christian Weiß (C.Weiß | Photo)!</p>
 		</div>
 	</div>
 	<div class="section">
 		<div class="hl">Abgelaufene Aktionen</div>
 	</div>
 	<div class="row">
-		<div class="box" style="float: right;">
+		<div class="box">
+			<a href="/forum" class="hl">Forum</a>
+			Alles Organisatorische, u.A. Rubriken für die Abizeitung.
+		</div>
+		<div class="box right">
+			<a href="/lists/official" class="hl">Planung offizieller Teil</a>
+			Verschiedene Infos, die zur Planung der offiziellen Verabschiedung benötigt werden.
+		</div>
+	</div>
+	<div class="row">
+		<div class="box">
+			<a href="/lists/song" class="hl">Einlauflied</a>
+			Sende dein persönliches Lied für die Zeugnisvergabe ein.
+		</div>
+		<div class="box right">
+			<a href="/shirts" class="hl">Abi-Shirt bestellen</a>
+			Bestelle hier dein Abi-Shirt.
+		</div>
+	</div>
+	<div class="row">
+		<div class="box">
 			{if $smarty.cookies.update < $_vars.update}<div class="update">Update!</div>{/if}
 			<a href="/profile" class="hl">Steckbrief</a>
 			Hier kannst du deinen eigenen Steckbrief für die Abizeitung ausfüllen.
 		</div>
-		<div class="box">
+		<div class="box right">
 			<a href="/pics" class="hl">Fotos für Collagen</a>
 			Lade deine Fotos für die Collagen in der Abizeitung hoch.
 		</div>
 	</div>
 	<div class="row">
-		<div class="box" style="float: right;">
+		<div class="box">
 			<a href="/stories" class="hl">Kursberichte</a>
 			Hier kannst du Kursberichte und die dazugehörigen Kursfotos hochladen.
-		</div>
-		<div class="box">
-			<a href="/shirts" class="hl">Abi-Shirt bestellen</a>
-			Bestelle hier dein Abi-Shirt.
 		</div>
 	</div>
 	{if $_vars.admin}
